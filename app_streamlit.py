665b1c6b44041c8f7804d19c40f4f63fafa744b3
rennequintero <151976838+rennequintero@users.noreply.github.com>
2023-12-01 20:58:39 -0500

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import sklearn
 from sklearn.preprocessing import LabelEncoder
 
 # Configuramos la página de Streamlit
